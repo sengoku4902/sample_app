353e05453be4428d3ac3071d6532373b89f6a50e
sengoku4902 <sasazaki4902@gmail.com>
2020-10-22 17:31:51 +0900

    commit2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'rails-sample-app-sasazaki7' # 作成したバケット名を記述
+    config.fog_directory  = 'rails-sample-app-sasazaki7'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id], # 環境変数
-      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key], # 環境変数
-      region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
+      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
+      region: 'ap-northeast-1',
       path_style: true
     }
 end
