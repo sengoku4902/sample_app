d728aad92c551e08f5580a459e4e809fd11a3d93
sengoku4902 <sasazaki4902@gmail.com>
2020-10-24 02:33:02 +0900

    commit4

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ threads threads_count, threads_count
 #
 # port        ENV.fetch("PORT") { 3000 }
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-
+daemonize
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
