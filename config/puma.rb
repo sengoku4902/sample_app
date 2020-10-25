4855c390e9b231a6a7df0aa1c08ea1c4ad59a3e9
sengoku4902 <sasazaki4902@gmail.com>
2020-10-26 04:37:16 +0900

    commit5

--- config/puma.rb
+++ config/puma.rb
@@ -9,9 +9,9 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port        ENV.fetch("PORT") { 3000 }
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-daemonize
+port        ENV.fetch("PORT") { 3000 }
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# daemonize
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
